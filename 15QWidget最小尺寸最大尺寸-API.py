9ad82f2a3912de501347f754fd880da6fd8c1aab
liyan <liyan@localhost.localdomain>
2022-08-12 17:28:53 +0800

    内容边界设置

--- 15QWidget最小尺寸最大尺寸-API.py
+++ 15QWidget最小尺寸最大尺寸-API.py
@@ -1,6 +1,6 @@
 import sys
 
-from PyQt5.QtWidgets import QWidget, QApplication
+from PyQt5.QtWidgets import QWidget, QApplication, QLabel
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
@@ -8,6 +8,15 @@ if __name__ == '__main__':
     w = QWidget()
     w.setWindowTitle("窗口大小控制")
     w.setMinimumSize(500, 500)
+
+    label = QLabel(w)
+    label.setText("社会我顺哥")
+    label.resize(300, 300)
+    label.setStyleSheet("background-color:cyan;")
+
+    label.setContentsMargins(100, 200, 0, 0)  # 文本内容距边框的距离
+    print(label.getContentsMargins())
+
 
     w.show()
 
