ac7b4e3a8b0d6206cb3c73f7f5d286d606d4d6af
liyan <1003955517@qq.com>
2022-08-13 23:14:58 +0800

    设置窗口状态

--- 18窗口相关操作.py
+++ 18窗口相关操作.py
@@ -1,6 +1,7 @@
 import sys
 
 from PyQt5.QtWidgets import QWidget, QApplication
+from PyQt5.QtCore import Qt
 
 
 class MyWindow(QWidget):
@@ -9,14 +10,21 @@ class MyWindow(QWidget):
         self.setup_ui()
 
     def setup_ui(self):
-        self.设置window的透明度()
-
+        # self.设置window的透明度()
+        self.设置窗口状态()
     def 设置window的透明度(self):
         self.setWindowTitle(" ")  # 设置空白标题
         self.setStyleSheet("background:#002EA6")
         self.setWindowOpacity(0.1)  # 设置不透明度
 
+    def 设置窗口状态(self):
+        print(self.windowState() == Qt.WindowNoState) # 默认为无状态
+        # self.setWindowState(Qt.WindowMaximized) # 设置最大化
+        # self.setWindowState(Qt.WindowMinimized) # 设置最小化
+        # self.setWindowState(Qt.WindowFullScreen) # 设置全屏
+        self.setWindowState(Qt.WindowActive) # 获取窗口活跃状态
 
+    def
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     w = MyWindow()
